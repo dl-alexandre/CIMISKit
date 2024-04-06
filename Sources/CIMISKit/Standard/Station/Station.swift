631ce5ff4c5220b16af8b5f3082a33fbbe0b8391
Dalton Alexandre <alexandrefamilyfarm@icloud.com>
2024-04-06 12:59:50 -0700

    More variables for Station Model

--- Sources/CIMISKit/Standard/Station/Station.swift
+++ Sources/CIMISKit/Standard/Station/Station.swift
@@ -1,15 +1,15 @@
-//
-//  File.swift
-//  
-//
-//  Created by Dalton Alexandre on 12/21/23.
-//
+ //
+ //  File.swift
+ //
+ //
+ //  Created by Dalton Alexandre on 12/21/23.
+ //
 
 import Foundation
 
 public struct Station: Codable, Identifiable, Hashable {
  public var id = UUID()
-    public let stationNbr: String
+ public let stationNbr: String
  public let name, city: String
  public let regionalOffice: RegionalOffice
  public let county, connectDate, disconnectDate: String
@@ -19,23 +19,39 @@ public struct Station: Codable, Identifiable, Hashable {
  public let hmsLatitude, hmsLongitude: String
  public let zipCodes: [String]
  public let sitingDesc: String
-
-    enum CodingKeys: String, CodingKey {
-        case stationNbr = "StationNbr"
-        case name = "Name"
-        case city = "City"
-        case regionalOffice = "RegionalOffice"
-        case county = "County"
-        case connectDate = "ConnectDate"
-        case disconnectDate = "DisconnectDate"
-        case isActive = "IsActive"
-        case isEtoStation = "IsEtoStation"
-        case elevation = "Elevation"
-        case groundCover = "GroundCover"
-        case hmsLatitude = "HmsLatitude"
-        case hmsLongitude = "HmsLongitude"
-        case zipCodes = "ZipCodes"
-        case sitingDesc = "SitingDesc"
-    }
+ 
+ public var latitude: String {
+  String(hmsLatitude[..<(hmsLatitude.firstIndex(of: " ") ?? hmsLatitude.endIndex)])
+ }
+ 
+ public var longitude: String {
+  String(hmsLongitude[..<(hmsLongitude.firstIndex(of: " ") ?? hmsLongitude.endIndex)])
+ }
+ 
+ public var location: CLLocation {
+  CLLocation(latitude: Double(latitude)!, longitude: Double(longitude)!)
+ }
+ 
+ public var locationDescription: String {
+  "\(city), \(county), \(stationNbr)"
+ }
+ 
+ enum CodingKeys: String, CodingKey {
+  case stationNbr = "StationNbr"
+  case name = "Name"
+  case city = "City"
+  case regionalOffice = "RegionalOffice"
+  case county = "County"
+  case connectDate = "ConnectDate"
+  case disconnectDate = "DisconnectDate"
+  case isActive = "IsActive"
+  case isEtoStation = "IsEtoStation"
+  case elevation = "Elevation"
+  case groundCover = "GroundCover"
+  case hmsLatitude = "HmsLatitude"
+  case hmsLongitude = "HmsLongitude"
+  case zipCodes = "ZipCodes"
+  case sitingDesc = "SitingDesc"
+ }
 }
 
